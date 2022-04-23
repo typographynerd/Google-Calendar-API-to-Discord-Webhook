065d1dd2633824ff1c04f0c49521f9bab8fe885f
Typography Nerd <pogo@bksl.ca>
2022-04-23 11:24:57 -0400

    Updated branch to latest main
    Re-set before making branch addtions

--- GoogleCalendarToDiscordWebhook.js
+++ GoogleCalendarToDiscordWebhook.js
@@ -36,71 +36,67 @@ function postEventsToChannel() {
         continue;
       }
 
-      // find and replace HTML tags with Discord Markdown
-      let descript = event.description
-        descript = descript.replace(/<ul>/g, "");
-        descript = descript.replace(/<\/ul>/g, "");
-        descript = descript.replace(/<li>/g, "");
-        descript = descript.replace(/<\/li>/g, "\n");
-        descript = descript.replace(/<u>/g, "__");
-        descript = descript.replace(/<\/u>/g, "__");
-        descript = descript.replace(/<b>/g, "**");
-        descript = descript.replace(/<\/b>/g, "**");
-        descript = descript.replace(/&nbsp;/g, " ");
-        descript = descript.replace(/<br>/g, "\n");
+      // script customizations start here
+            // find and replace HTML tags with Discord Markdown
+            let descript = event.description
+              descript = descript.replace(/<ul>/g, "");
+              descript = descript.replace(/<\/ul>/g, "");
+              descript = descript.replace(/<li>/g, "");
+              descript = descript.replace(/<\/li>/g, "\n");
+              descript = descript.replace(/<u>/g, "__");
+              descript = descript.replace(/<\/u>/g, "__");
+              descript = descript.replace(/<b>/g, "**");
+              descript = descript.replace(/<\/b>/g, "**");
+              descript = descript.replace(/&nbsp;/g, " ");
+              descript = descript.replace(/<br>/g, "\n");
 
-      // Build the POST request
-      let options = {
-          "method": "post",
-          "headers": {
-              "Content-Type": "application/json",
-          },
-          "payload": JSON.stringify({
-              "content": "‌",
-              "embeds": [{
-              "author": {
-                  "name": `${event.summary}`,
-                  "icon_url": "https://cdn.discordapp.com/attachments/696400605908041794/888874282950750238/1200px-Google_Calendar_icon_28202029.png"
-              },
-                "timestamp": DTnow.toISO(),
-                "description":`[Google Event Link](${event.htmlLink})`,
-                "color": 1425196,
-                "fields":[
-                    {
-                      "name":"Start Time",
-                      "value": ISOToDiscordUnix(ISOStartDate) ?? NO_VALUE_FOUND,
-                      "inline":false
+            // Build the POST request
+            let options = {
+                "method": "post",
+                "headers": {
+                    "Content-Type": "application/json",
+                },
+                "payload": JSON.stringify({
+                    "content": "‌",
+                    "embeds": [{
+                    "author": {
+                        "name": `${event.summary}`,
+                        "icon_url": "https://cdn.discordapp.com/attachments/696400605908041794/888874282950750238/1200px-Google_Calendar_icon_28202029.png"
                     },
-                    {
-                      "name":"End Time",
-                      "value":ISOToDiscordUnix(ISOEndDate) ?? NO_VALUE_FOUND,
-                      "inline":false
-                    },
-                    {
-                      "name":"Location",
-                      "value":event.location ?? NO_VALUE_FOUND,
-                      "inline":false
-                    },
-                    {
-                      "name":"Description",
-                      "value":event.description ?? NO_VALUE_FOUND,
-                      "inline":false
-                    }
-                ]
-            }]
-          })
-      };
-      Logger.log(options, null, 2);
-      UrlFetchApp.fetch(CHANNEL_POST_URL, options);
-    }
-  } else {
-    Logger.log(`No events starting within ${minsInAdvance} minute(s) found.`);
-  }
-}
+                      "timestamp": DTnow.toISO(),
+                      "description":`[Google Event Link](${event.htmlLink})`,
+                      "color": 1425196,
+                      "fields":[
+                          {
+                            "name":"Start Time",
+                            "value": ISOToDiscordUnix(ISOStartDate) ?? NO_VALUE_FOUND,
+                            "inline":false
+                          },
+                          {
+                            "name":"End Time",
+                            "value":ISOToDiscordUnix(ISOEndDate) ?? NO_VALUE_FOUND,
+                            "inline":false
+                          },
+                          {
+                            "name":"Description",
+                            "value":descript ?? NO_VALUE_FOUND,
+                            "inline":false
+                          }
+                      ]
+                  }]
+                })
+            };
+            Logger.log(options, null, 2);
+            UrlFetchApp.fetch(CHANNEL_POST_URL, options);
+          }
+        } else {
+          Logger.log(`No events starting within ${minsInAdvance} minute(s) found.`);
+        }
+      }
 
-/**
- * Converts an ISO string into a discord formatted timestamp
- */
-function ISOToDiscordUnix(isoString) {
-  return `<t:${Math.floor(DateTime.fromISO(isoString).toSeconds())}:F>`
-}
+      /**
+       * Converts an ISO string into a discord formatted timestamp
+       */
+      function ISOToDiscordUnix(isoString) {
+        return `<t:${Math.floor(DateTime.fromISO(isoString).toSeconds())}:F>`
+      }
